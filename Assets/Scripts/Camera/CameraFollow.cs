c23ac424414b2c72c39507961be7329d24296c8c
efdalyagan <119241162+efdalyagan@users.noreply.github.com>
2025-05-05 14:24:19 +0300

    level1_finished

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(1f, 3.5f, -10f);
+    private Vector3 offset = new Vector3(1f, 0f, -10f);
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
     [SerializeField] private Transform target;
